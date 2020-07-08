19db73b6f09b317bdcde36ae09f3dcd1360ef369
Mike Boring <shekinah999@msn.com>
2020-07-08 15:56:02 -0600

    Passed all tests for assessment

--- dirwatcher.py
+++ dirwatcher.py
@@ -17,23 +17,6 @@ for a "magic string", which is provided as a command line argument.
  - Use exception handling to keep the program running
  - Structure your code repository using best practices
  - Read a set of requirements and deliver on them, asking for clarification if anything is unclear
-
-TODO:
-
-DONE 1. accept command line arguments with argparse
-        add argument for txt file to monitor, magic word to look for
-DONE 2. monitor a given directory for text files created in that directory
-        timed polling loop with sleep to check directory
-3. If the magic string is found in a file, your program should log a message
-        indicating which file, and the line number within the file where the magic text
-        was found. Just one magic string location logged per line.
-DONE 4. program should terminate itself when catching SIGTERM or SIGINT signals
-        (be sure to log a termination message). OS signal handler
-5. Handle and log different exceptions such as "file not found", "directory does not exist",
-        as well as handle and report top-level unknown exceptions so that your program stays alive
-DONE 6. Include a startup and shutdown banner in your logs and report the total runtime (uptime)
-        within your shutdown log banner
-
 """
 
 import os
@@ -59,7 +42,6 @@ def create_parser():
         '-e', '--ext', help='Text file extension to watch.')
     parser.add_argument('-i', '--interval', type=int,
                         help='Number of seconds between polling.', default=3)
-
     return parser
 
 
@@ -88,21 +70,20 @@ def signal_handler(sig_num, frame):
         ----------------------------------------------------
         """
     )
-
     exit_flag = True
 
 
 def start_watch_directory(watch_directory, magic_text, polling_interval, ext):
-    """Watch a directory for new files being added and scan for magic word."""
+    """Watch a directory for new files being added , deleted and scan for magic word."""
     if not os.path.isdir(f'{watch_directory}'):
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.ERROR)
         logger.error(
             f'Directory or file not found:{os.getcwd()}/{watch_directory}')
     else:
-        scan_full_directory(watch_directory, ext, magic_text)
         detect_deleted_file(watch_directory)
         detect_added_files(watch_directory, ext, magic_text)
+        scan_full_directory(watch_directory, ext, magic_text)
 
 
 def detect_added_files(watch_directory, ext, magic_text):
@@ -114,9 +95,10 @@ def detect_added_files(watch_directory, ext, magic_text):
     else:
         for f in new_directory_list:
             if f.endswith(ext) and f not in current_directory_dict:
-                print(f'New file detected: {f}')
+                logger = logging.getLogger(__name__)
+                logger.setLevel(logging.INFO)
+                logger.info(f'New file detected: {f}')
                 current_directory_dict[f] = 0
-                # print('new current dictionary: ', current_directory_dict)
                 scan_single_file(watch_directory, f, magic_text)
             else:
                 continue
@@ -132,19 +114,15 @@ def detect_deleted_file(watch_directory):
         for dict_entry in current_directory_dict:
             if dict_entry not in new_directory_list:
                 del current_directory_dict[dict_entry]
-                print('Deleted File Detected and Removed from Directory Dict')
-    # if files deleted from directory, then log message
-    # logging.basicConfig(level=logging.WARNING)
-    # logger = logging.getLogger(__name__)
-    # logger.warning('Detected deleted file.')
+                logger = logging.getLogger(__name__)
+                logger.setLevel(logging.INFO)
+                logger.info('Files were deleted.')
 
 
 def scan_single_file(watch_directory, file_name, magic_text):
     """Scan a file for the specified magic word."""
     with open(f'{watch_directory}/{file_name}') as f:  # open individual file
-        #text = f.read()
         for i, line in enumerate(f):
-            # loop thru and read line by line looking for magic word, then keep track of line in overall wtched dictionary. enumerate
             if magic_text in line.lower():
                 logger = logging.getLogger(__name__)
                 logger.setLevel(logging.INFO)
@@ -152,7 +130,6 @@ def scan_single_file(watch_directory, file_name, magic_text):
                 global current_directory_dict
                 current_directory_dict[file_name] = i+1
             else:
-                #print('scan found no magic text')
                 continue
 
 
@@ -162,20 +139,14 @@ def scan_full_directory(watch_directory, ext, magic_text):
     if os.path.isdir(f'{watch_directory}'):  # confirm directory exists
         # creates list of directory contents
         new_directory_list = os.listdir(watch_directory)
-        if not len(new_directory_list) == 0:  # confirm there is a file on the directory
-            #print('new directory list ln 155: ', new_directory_list)
+        if not len(new_directory_list) == 0:  # confirm there is a file in the directory
+
             for single_file in new_directory_list:
                 if single_file.endswith(ext):  # sort files ending in ext
                     with open(f'{watch_directory}/{single_file}') as f:  # open individual file
-                        # print(
-                        #     f'current global directory dict ln 160: {current_directory_dict} - {datetime.datetime.now()}')
-                        #text = f.read()
                         for i, line in enumerate(f):
-                            # loop thru and read line by line looking for magic word, then keep track of line in overall wtched dictionary. enumerate
                             if magic_text in line.lower():
                                 if single_file in current_directory_dict:
-                                    # print(
-                                    #     f'current_directory_dict[single_file]: ', current_directory_dict[single_file])
                                     if current_directory_dict[single_file] < i+1:
                                         logger = logging.getLogger(__name__)
                                         logger.setLevel(logging.INFO)
@@ -188,7 +159,7 @@ def scan_full_directory(watch_directory, ext, magic_text):
 
 
 def main(args):
-    """Parses args, scans directory for magic words then logs results then repeats scan according to interval."""
+    """Scans directory for magic words then logs results then repeats scan according to interval."""
     global start_time
     start_time = datetime.datetime.now()
     logger = logging.getLogger(__name__)
@@ -207,7 +178,6 @@ def main(args):
         sys.exit(1)
 
     parsed_args = parser.parse_args(args)
-    #print('parsed_args', parsed_args)
     ext = parsed_args.ext
     polling_interval = parsed_args.interval
     magic_text = parsed_args.magic.lower()
@@ -223,7 +193,9 @@ def main(args):
                 if f.endswith(ext):                 # first scan of directory and add
                     # those with right ext to global current_directory_dict
                     current_directory_dict[f] = 0
-                    print(f'New file detected: {f}')
+                    logger = logging.getLogger(__name__)
+                    logger.setLevel(logging.INFO)
+                    logger.info(f'New file detected: {f}')
                     # scan individual file for magic text
                     scan_single_file(watch_directory, f, magic_text)
 
